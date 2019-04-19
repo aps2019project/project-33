977853b436ce5f64ce67eae1df1282ee7351e9a2
mhdmohammadi <m.mohammadi1378@outlokk.com>
2019-04-19 19:36:13 +0430

    Shop: Buy Notes: some Changes

--- src/Shop.java
+++ src/Shop.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.Collection;
 import java.util.Scanner;
 
 public class Shop extends Menu {
@@ -35,6 +37,39 @@ public class Shop extends Menu {
             this.help();
         else
             System.out.println("Please enter valid command line !");
+    }
+
+    public void buy(String name){
+        CollectionItem collectionItem = this.collection.getCollectionItemByName(name);
+        if(collectionItem == NULL){
+            System.out.println("There isn't this thing in collection");
+        }
+        if(collectionItem instanceof UsableItem) this.buyItem((UsableItem)collectionItem);
+        else this.buyCard((Card) collectionItem);
+    }
+
+    public void buyItem(UsableItem item){
+        if(item.getPrice() > Main.application.getLoggedInAccount().getBudget()){
+            System.out.println("Low Budget !!");
+            return;
+        }
+        if(Main.application.getLoggedInAccount().getNumberOfItems == 3){
+            System.out.println("You have 3 Items! Please sell at least 1 item at first !");
+            return;
+        }
+        this.collection.removeCardFromCollection(item.getName());
+        Main.application.getLoggedInAccount().decreaseBudget(item.getPrice());
+        Main.application.getLoggedInAccount().getCollection().addCardToCollection(item.getName());
+    }
+
+    public void buyCard(Card card){
+        if(card.getPrice() > Main.application.getLoggedInAccount().getBudget()){
+            System.out.println("Low Budget!");
+            return;
+        }
+        this.collection.removeCardFromCollection(card.getName());
+        Main.application.getLoggedInAccount().decreaseBudget(card.getPrice());
+        Main.application.getLoggedInAccount().getCollection().addCardToCollection(card.Name);
     }
 
     //Here is Setters && Getters
