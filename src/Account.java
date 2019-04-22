53a58b0576f67e07581d792e54c958694c7daef8
mehregan mohseni <mehreganmohseni@gmail.com>
2019-04-22 16:03:04 +0430

    Class Player(with setter && getter / without attack)

---
+++ src/Account.java
@@ -0,0 +1,18 @@
+
+
+
+public class Account {
+    private static Arraylist<Account> acccounts = new Arraylist<Account>;
+    private String username;
+    private String password;
+    private Collection collection;
+    private int budget;
+    private int numberOfItems;
+    private Arraylist<Match> matches = new Arraylist<Match>;
+
+
+
+
+
+
+}
--- src/Player.java
+++ src/Player.java
@@ -2,15 +2,95 @@
 
 
 public class Player {
+    public CollectionItem getSelectedCollectionItem() {
+        return selectedCollectionItem;
+    }
+
+    public void setSelectedCollectionItem(CollectionItem selectedCollectionItem) {
+        this.selectedCollectionItem = selectedCollectionItem;
+    }
+
     private CollectionItem selectedCollectionItem;
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
     private Hand hand;
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public void setHero(Hero hero) {
+        this.hero = hero;
+    }
+
     private Hero hero;
+
+    public int getNumberOfFlags() {
+        return numberOfFlags;
+    }
+
+    public void setNumberOfFlags(int numberOfFlags) {
+        this.numberOfFlags = numberOfFlags;
+    }
+
     private int numberOfFlags;
+
+    public int getFlagTurns() {
+        return flagTurns;
+    }
+
+    public void setFlagTurns(int flagTurns) {
+        this.flagTurns = flagTurns;
+    }
+
     private int flagTurns;
+
+    public Cell getHeroPosition() {
+        return heroPosition;
+    }
+
+    public void setHeroPosition(Cell heroPosition) {
+        this.heroPosition = heroPosition;
+    }
+
     private Cell heroPosition;
+
+    public Mana getMana() {
+        return mana;
+    }
+
+    public void setMana(Mana mana) {
+        this.mana = mana;
+    }
+
     private Mana mana;
+
+    public Arraylist<CollectionItem> getUsedCards() {
+        return usedCards;
+    }
+
     private Arraylist<CollectionItem> usedCards = new Arraylist<CollectionItem>;
+
+    public void addNewUsedCards(CollectionItem collectionItem){
+        usedCards.add(collectionItem);
+    }
+
+    public Arraylist<CollectableItem> getCollectableItems() {
+        return collectableItems;
+    }
+
     private Arraylist<CollectableItem> collectableItems = new Arraylist<CollectionItem>;
+
+    public void addNewCollectableItems(CollectableItem collectableItem){
+        collectableItems.add(collectableItem);
+    }
 
     public void insertCard(Map map, String cardName, int x, int y) {
         map.insertCard(cardName, x, y);
