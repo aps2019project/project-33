390dcdc51eb18fc6d4d9be94d9e6c38016de3a03
mhdmohammadi <m.mohammadi1378@outlokk.com>
2019-04-26 12:26:39 +0430

    GraveYard UML implementation

---
+++ src/GraveYard.java
@@ -0,0 +1,20 @@
+public class GraveYard {
+    private ArrayList<Card> cards;
+
+    public void addCard(Card card){}
+    public Card SearchCard(String cardID){}
+    public void showInfo(String cardID){}
+    public void showCards(){}
+    public void inputCommandLine(){}
+    public void main(){}
+
+    //Here is Setters && Getters
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+}
