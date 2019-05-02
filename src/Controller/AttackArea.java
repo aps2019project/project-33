147ca59edb33dea70b8b7e9a2caceff58f63abaf
mhdmohammadi <m.mohammadi1378@outlook.com>
2019-05-03 01:45:26 +0430

    Attack area ha va impact ha

---
+++ src/Controller/AttackArea.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import Model.CollectionItem.Hero;
+import Model.CollectionItem.LivingCard;
+import Model.CollectionItem.Spell;
+import Model.Enviroment.Cell;
+
+import java.util.ArrayList;
+
+public class AttackArea {
+    public static ArrayList<Cell> getImapctCellsOfSpecialPower(LivingCard livingCard) {
+        return null;
+    }
+
+    public static ArrayList<Cell> getImpactCellsOfAttack(LivingCard livingCard) {
+        return null;
+    }
+
+    public static ArrayList<Cell> getImpactCellsOfCounterAttack(LivingCard livingCard) {
+        return null;
+    }
+
+    public static ArrayList<Cell> getImpactSellOfSpell(Spell spell, Cell coordination) {
+        return null;
+    }
+}
---
+++ src/Controller/Impact.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import Model.CollectionItem.*;
+import Model.Enviroment.Cell;
+
+public class Impact {
+
+    public static void attack(LivingCard attacker, LivingCard defender){}
+
+    public static void counterAttack(LivingCard livingCard, LivingCard opponentCard) {
+
+    }
+
+    public static void specialPower(Minion minion, LivingCard opponentCard) {
+    }
+
+    public static void impactItem(Item item, String livingCardID) {
+    }
+
+    public static void impactSpellOfHero(String opponentID) {
+    }
+
+    public static void impactSpell(Spell spell, Cell cell) {
+    }
+}
