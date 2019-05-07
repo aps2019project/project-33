29ae8df02d6c76b22699e80869daa40329430200
mhdmohammadi <m.mohammadi1378@outlook.com>
2019-05-08 04:03:38 +0430

    create collection item done

--- src/Model/CollectionItem/Item.java
+++ src/Model/CollectionItem/Item.java
@@ -1,28 +1,31 @@
 package Model.CollectionItem;
 
+import Controller.Application;
 import Controller.Impact;
 import Model.Collection;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public  class Item extends CollectionItem {
 
+    public static Item createItem(String itemName, String playerName) throws FileNotFoundException {
+        String address = "Data/CollectionItem/Spell" + itemName + ".json";
+        Item item = (Item) Application.readJSON(Item.class, address);
 
-/*
-    public static Item createItem(String playerName, String itemName){
-        Item item = loadItemFromJsonFile(itemName);
         int numberOfThisItemType = 0;
         ArrayList<Item> usedItems = CollectionItem.getAllItems();
         for(Item usedItem : usedItems){
             if(usedItem.getName().equals(itemName))
                 numberOfThisItemType++;
         }
+
         String ID = playerName + "_" + itemName + "_" + numberOfThisItemType;
         item.setID(ID);
         CollectionItem.addItemToAllItems(item);
         return item;
     }
-*/
+
 
    /* @Override
     public void doImpact(String livingCardID) {
