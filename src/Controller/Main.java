6c3eef58d4c3ccd6384abff2eb82d1f8eca7c6c8
alirezaisavand <isavand.alireza@gmail.com>
2019-05-07 18:51:21 +0430

    total disarm + json

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,9 +1,10 @@
 package Controller;
 
 import Model.CollectionItem.Spell;
+import com.gilecode.yagson.YaGson;
+import com.gilecode.yagson.YaGsonBuilder;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -24,6 +25,32 @@ public class Main {
         spell.getInformation().setCanDisarmBuffAdd(true);
         spell.getInformation().setDisarmBuffPermanent(true);
         spell.setName("Total Disarm");
+        writeJSON(spell, "Data/CollectionItem/Spell/TotalDisarm.json");
+    }
+
+    public static Object copy(Object object, Class className) throws IOException {
+        String address = "copy.json";
+        writeJSON(object, address);
+        Object copyObject = readJSON(className, address);
+        return copyObject;
+    }
+
+    public static Object readJSON(Class className, String address) throws FileNotFoundException {
+        YaGsonBuilder builder = new YaGsonBuilder();
+        YaGson yaGson = builder.create();
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader(address));
+
+        Object object = yaGson.fromJson(bufferedReader, className);
+        return object;
+    }
+
+    public static void writeJSON(Object object, String address) throws IOException {
+        YaGsonBuilder builder = new YaGsonBuilder();
+        YaGson yaGson = builder.create();
+        FileWriter writer = new FileWriter(address);
+        writer.write(yaGson.toJson(object));
+        writer.close();
     }
 }
 
