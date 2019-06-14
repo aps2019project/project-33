c543f970b629895485878e48cb9e08e55d10823d
mehregan mohseni <mehreganmohseni@gmail.com>
2019-06-15 00:11:53 +0430

    BattleMenuController

---
+++ src/View/BattleMenu/BattleMenuController.java
@@ -0,0 +1,28 @@
+package BattleMenu;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BattleMenuController implements Initializable {
+
+
+    public Label backButton;
+    public Label chapterOneButton;
+    public Label chapterTwoButton;
+    public Label storyButton;
+    public Label chapterThreeButton;
+    public Label customGameButton;
+    public Label kindOneButton;
+    public Label kindThreeButton;
+    public Label kindTwoButton;
+    public Label singlePlayerButton;
+    public Label multiPlayerButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
