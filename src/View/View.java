4d2c4706ecc032fdbff12dc1333c57784cc20d75
mehregan mohseni <mehreganmohseni@gmail.com>
2019-06-15 13:50:19 +0430

    correct address of files

--- src/View/View.java
+++ src/View/View.java
@@ -10,7 +10,7 @@ public class View extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AccountMenu/AccountMenu.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
