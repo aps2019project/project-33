0fcbfa723854e405baeeb1c12682b4a95cd9c7d3
mhdmohammadi <momohammadi@ce.sharif.edu>
2019-06-29 18:50:23 +0430

    nmd

--- src/Controller/MenuList.java
+++ src/Controller/MenuList.java
@@ -3,5 +3,15 @@ package Controller;
 public enum MenuList {
     AccountMenu, MainMenu, ShopMenu, CollectionMenu, ChooseType, ChooseMode, ChooseKind, ChooseChapter, Battle;
 
+    public String getAddressOfFile() {
+        if(this.equals(AccountMenu)) return "AccountMenu/AccountMenu.fxml";
+        if(this.equals(MainMenu)) return "MainMenu/MainMenu.fxml";
+        if(this.equals(ShopMenu)) return "ShopMenu/ShopMenu.fxml";
+        if(this.equals(CollectionMenu)) return "CollectionMenu/CollectionMenu.fxml";
+        if(this.equals(ChooseType)) return "BattleMenu/ChooseType.fxml";
+        if(this.equals(ChooseMode)) return "BattleMenu/ChooseMode.fxml";
+        if(this.equals(ChooseKind)) return "BattleMenu/ChooseKind.fxml";
+        if(this.equals(ChooseChapter)) return "BattleMenu/ChooseChapter.fxml";
+    }
 }
 
--- src/View/View.java
+++ src/View/View.java
@@ -1,10 +1,16 @@
 package View;
 
+import Controller.Client;
+import Controller.MenuList;
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Stack;
 
 public class View extends Application {
     @Override
@@ -14,8 +20,30 @@ public class View extends Application {
         primaryStage.setScene(new Scene(root));
 
 
+        Task task = new Task() {
+            private MenuList previousMenu = MenuList.AccountMenu;
+            @Override
+            protected Object call() throws Exception {
+                if(!Client.getClient().getCurrentMenu().equals(previousMenu)){
+                    previousMenu = Client.getClient().getCurrentMenu();
+                    showMenu(primaryStage, previousMenu);
+                }
+                Thread.sleep(200);
+                return null;
+            }
+        };
+
 
         primaryStage.show();
+    }
+
+    private void showMenu(Stage primaryStage, MenuList newMenu) throws IOException {
+        primaryStage.setTitle(newMenu.toString());
+        String address = newMenu.getAddressOfFile();
+
+        Parent root = FXMLLoader.load(getClass().getResource(address));
+        primaryStage.setScene(new Scene(root));
+
     }
 
     public static void main(String[] args){
