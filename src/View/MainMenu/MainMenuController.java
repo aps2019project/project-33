167ce6d73086a020cc1afee9bb50fb29d3b05f08
alirezaisavand <isavand.alireza@gmail.com>
2019-06-29 21:51:17 +0430

    main menu is completing

--- src/View/MainMenu/MainMenuController.java
+++ src/View/MainMenu/MainMenuController.java
@@ -1,25 +1,68 @@
 package View.MainMenu;
 
+import Controller.Client;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
     public AnchorPane anchorPane;
     public VBox vBox;
-    public Label play;
+    public Label battle;
     public Label shop;
     public Label collection;
     public ImageView duelyst;
     public ImageView logout;
+    public Label save;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        collection.setOnMouseClicked(event -> {
 
+            try {
+                Client.getClient().getMainMenu().inputCommandLine("enter collection");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        logout.setOnMouseClicked(event -> {
+            try {
+                Client.getClient().getMainMenu().inputCommandLine("logout");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        shop.setOnMouseClicked(event -> {
+            try {
+                Client.getClient().getMainMenu().inputCommandLine("enter shop");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        battle.setOnMouseClicked(event -> {
+            try {
+                Client.getClient().getMainMenu().inputCommandLine("enter battle");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        save.setOnMouseClicked(event -> {
+            try {
+                Client.getClient().getMainMenu().inputCommandLine("save");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
