32379a3ddbb8c961920aaf38968aa3d2c0145c6b
mehreganmohseni <45387412+mehreganmohseni@users.noreply.github.com>
2019-07-06 04:14:04 +0430

    chat

---
+++ src/Model/Massage.java
@@ -0,0 +1,40 @@
+package Model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Massage implements Serializable {
+    private static ArrayList<Massage> massages = new ArrayList<>();
+    private String massageText;
+    private Account speaker;
+
+    public Massage(String massageText, Account speaker) {
+        this.massageText = massageText;
+        this.speaker = speaker;
+    }
+
+    public synchronized static void addMassage(Massage massage) {
+        massages.add(massage);
+    }
+
+    public String getMassageText() {
+        return massageText;
+    }
+
+    public void setMassageText(String massageText) {
+        this.massageText = massageText;
+    }
+
+    public Account getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(Account speaker) {
+        this.speaker = speaker;
+    }
+
+    public static ArrayList<Massage> getMassages() {
+        return massages;
+    }
+
+}
--- src/View/Chat/ChatController.java
+++ src/View/Chat/ChatController.java
@@ -1,13 +1,71 @@
 package View.Chat;
 
+import Controller.Client.Client;
+import Controller.MenuList;
+import Model.Massage;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ChatController implements Initializable {
+    public Label backButton;
+    public TextArea massageField;
+    public ImageView sentButton;
+    public VBox chatArea;
+
+    private AnimationTimer animationTimer;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        backButton.setOnMouseClicked(event -> {
+            animationTimer.stop();
+            try {
+                Client.getClient().changeCurrentMenu(MenuList.MainMenu);
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
 
+        sentButton.setOnMouseClicked(event -> {
+            if(massageField.getText().isEmpty()) return;
+            String massageText = massageField.getText();
+            try {
+                Client.getClient().sendMassageInChat(massageText);
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
+
+        animationTimer = new AnimationTimer() {
+            long last = 0, unit = 1000000000, fps = 10;
+            @Override
+            public void handle(long now) {
+                if(last == 0) last = now;
+                if(now > last + unit / fps){
+                    chatArea.getChildren().clear();
+                    ArrayList<Massage> massages = null;
+                    try {
+                        massages = Client.getClient().getAllMassages().getMassages();
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    for(Massage massage : massages){
+                        Label label = new Label();
+                        label.setText(massage.getSpeaker().getUsername() + " : " + massage.getMassageText());
+                        label.getStyleClass().add("ChatMassage");
+                        chatArea.getChildren().add(label);
+                    }
+                }
+            }
+        };
+        animationTimer.start();
     }
 }
