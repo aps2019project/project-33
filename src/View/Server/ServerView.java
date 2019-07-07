3eba113aeffe3d0718702b74c7613bbf127b128a
mehreganmohseni <mehreganmohseni@gmail.com>
2019-07-07 18:59:50 +0430

    ba ham zadim namoosan

---
+++ src/View/Server/ServerView.java
@@ -0,0 +1,31 @@
+package View.Server;
+
+import Controller.Client.Client;
+import Controller.MenuList;
+import View.Battle.BattleController;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ServerView extends Application {
+    private static Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("ShowClients");
+        Parent root = FXMLLoader.load(getClass().getResource("ShowClients.fxml"));
+        primaryStage.setScene(new Scene(root));
+        ServerView.primaryStage = primaryStage;
+
+        primaryStage.show();
+    }
+}
